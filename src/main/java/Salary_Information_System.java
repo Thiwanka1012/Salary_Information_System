d637310452610f00c2aa716cbd963c4d970c7278
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-08-01 18:19:41 +0530

    Add user interface prompts for salary calculations

--- src/main/java/Salary_Information_System.java
+++ src/main/java/Salary_Information_System.java
@@ -4,5 +4,18 @@ public class Salary_Information_System {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 
+        System.out.println("---------------------------------------------------");
+        System.out.println(" |           Salary Information System            |");
+        System.out.println("---------------------------------------------------");
+
+
+        System.out.println("1)Calculate Income Tax:");
+        System.out.println("2)Calculate Annual Bonus:");
+        System.out.println("3)Calculate Loan Amount:");
+
+        System.out.print("Please Select Option(1,2,3):");
+        int option=input.nextInt();
+        
+
     }
 }
